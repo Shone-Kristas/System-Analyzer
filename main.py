ad1ebb0d7a0ed331c4fabee4d2239705bfd18896
Nick Bold <kristas21@yandex.ru>
2024-02-12 16:29:21 +0100

    add exeption

--- main.py
+++ main.py
@@ -20,39 +20,42 @@ def file_permission(scan_file: str) -> dict:
     return permissions_dict
 
 def search(path: str) -> dict:
-    files_and_directories = os.listdir(path)
-    for i in range(len(files_and_directories)):
-        scan_file = path + '/' + files_and_directories[i]
-        directory = os.path.isdir(scan_file)
-        file_permission(scan_file)
-        if directory == True:
-            try:
-                search(scan_file)
-            except PermissionError:
-                print("Нет доступка к папке:", scan_file)
-            except Exception as e:
-                print("Произошла ошибка при открытии файла:", e)
-        elif directory == False:
-            try:
-                with open(scan_file, "rb") as f:
-                    extension = magic.from_buffer(f.read(1024), mime=True)
-                    file_size = os.path.getsize(scan_file)
-                    if extension in categories_dict:
-                        categories_dict[extension].append(files_and_directories[i])
-                        sizes_dict[extension] += file_size       #Размер файлов
-                    else:
-                        categories_dict[extension] = [files_and_directories[i]]
-                        sizes_dict[extension] = file_size
-                    if files_and_directories[i] in higher_threshold_dict and threshold_value < file_size:
-                        higher_threshold_dict[files_and_directories[i]] += file_size
-                    elif threshold_value < file_size:
-                        higher_threshold_dict[files_and_directories[i]] = file_size
-            except FileNotFoundError:
-                print("Файл не найден")
-            except Exception as e:
-                print("Произошла ошибка при открытии файла:", e)
+    try:
+        files_and_directories = os.listdir(path)
+        for i in range(len(files_and_directories)):
+            scan_file = path + '/' + files_and_directories[i]
+            directory = os.path.isdir(scan_file)
+            file_permission(scan_file)
+            if directory == True:
+                try:
+                    search(scan_file)
+                except PermissionError:
+                    print("Нет доступка к папке:", scan_file)
+                except Exception as e:
+                    print("Произошла ошибка при открытии файла:", e)
+            elif directory == False:
+                try:
+                    with open(scan_file, "rb") as f:
+                        extension = magic.from_buffer(f.read(1024), mime=True)
+                        file_size = os.path.getsize(scan_file)
+                        if extension in categories_dict:
+                            categories_dict[extension].append(files_and_directories[i])
+                            sizes_dict[extension] += file_size       #Размер файлов
+                        else:
+                            categories_dict[extension] = [files_and_directories[i]]
+                            sizes_dict[extension] = file_size
+                        if files_and_directories[i] in higher_threshold_dict and threshold_value < file_size:
+                            higher_threshold_dict[files_and_directories[i]] += file_size
+                        elif threshold_value < file_size:
+                            higher_threshold_dict[files_and_directories[i]] = file_size
+                except FileNotFoundError:
+                    print("Файл не найден")
+                except Exception as e:
+                    print("Произошла ошибка при открытии файла:", e)
 
-    return categories_dict, sizes_dict
+        return categories_dict, sizes_dict
+    except Exception as e:
+        print("Произошла ошибка:", e)
 
 
 search(path)

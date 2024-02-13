7e0a4aaa923ecf9a572c6dfca783aac51060901f
Nick Bold <kristas21@yandex.ru>
2024-02-14 00:38:52 +0100

    add readme

---
+++ README.md
@@ -0,0 +1,19 @@
+System-Analyzer is a command-line tool designed to analyze Linux file structure:
+* Classify files on type categories based on file content. 
+* Calculates and displays the total size for each file type category and identifies files above a certain size.
+* Generates a report of files with unusual permission settings.
+
+To run the System-Analyzer tool:
+* Clone the repository 
+* Install dependencies: 
+  ```
+  pip install python-magic
+  ```
+* Run the tool: 
+  ```
+  python main.py {path} {value}
+  ```
+  with set options:
+  * {path} - full path to the directory you want to scan 
+  * {value} - set a certain size in bytes. Files with a size above this value will be put into a report. 
+
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,53 +1,13 @@
 from src.main import file_permission
 import os
-import pytest
-
-
-
-# def my_fakefs_test(fs):
-#     # "fs" is the reference to the fake file system
-#     fs.create_file("/var/data/xx1.txt")
-#     assert os.path.exists("/var/data/xx1.txt")
-# def test_file_permission(fs):
-#     permissions_dict = {}
-#     permissions_dict = file_permission(file_path, permissions_dict)
-#     assert permissions_dict() == {'777': [file_path]}
-# @pytest.fixture()
-# def my_fakefs_test(fs):
-#     # "fs" is the reference to the fake file system
-#     filename = fs.create_file("C://xx1.txt", perm=0o777)
-#     return filename
-# def test_file_permission():
-#     permissions_dict = {}
-#     permissions_dict = file_permission(my_fakefs_test, permissions_dict)
-#     assert permissions_dict() == {'777': [my_fakefs_test]}
-
-# @pytest.mark.parametrize(
-#     "filename, permission",
-#     ["file1.txt", 0o777]
-# )
-# def test_file_permission(filename, permission):
-#     permissions_dict = {}
-#     permissions_dict = file_permission(filename, permissions_dict)
-#     assert permissions_dict() == {'777': [filename]}
-
 
 def test_file_permission():
-    # Test case 2: check file with permission 644
+    # Test case: check file with permission 444
     permissions_dict = {}
     filename = "file1.txt"
     os.chmod(filename, 0o444)
-    permission = oct(os.stat(filename).st_mode)[-3:]
-    assert permission == "444"
-    # permissions_dict = file_permission(filename, permissions_dict)
-    # assert permissions_dict == {'777': [filename]}
-# test_file_permission()
-# # Test case 3: check file with permission 000
-# filename = "file3.txt"
-# os.chmod(filename, 0o000)
-# permissions_dict = file_permission(filename, permissions_dict)
-# assert permissions_dict == {'777': [filename], '644': [filename], '000': [filename]}
+    permissions_dict = file_permission(filename, permissions_dict)
+    assert permissions_dict == {'444': [filename]}
 
-# print("All tests passed")
 
 
